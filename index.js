73f22ccf3d7163b4cbda41ed45770871c2f1bf9a
Rowan Crawford <rowan.crawford@joukou.co.nz>
2014-06-10 15:10:47 +1200

    Refactor to be a queue.

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ var async = require('async'),
     feedUrl,
     moment = require('moment'),
     startup = Date.now(),
+    latest = 0,
     twitter = require('ntwitter'),
     twit,
-    delay;
+    delay,
+    queue;
 
 twit = twitter({
   consumer_key: process.env.CONSUMER_KEY,
@@ -29,9 +31,16 @@ try {
 
 feedUrl = url.parse(process.env.FEED_URL);
 
-function poll() {
-  var latest = startup;
+queue = async.queue(processEntry);
+queue.drain = function() {
+  if (latest > startup) {
+    startup = latest;
+  }
+  console.log('polling again in %s minutes for events after %s ...', delay, moment(startup).format('LLL'));
+  setTimeout(poll, delay * 60 * 1000);
+};
 
+function poll() {
   request({
     json: true,
     uri: process.env.FEED_URL + '.json'
@@ -40,58 +49,54 @@ function poll() {
 
     delay = delay || process.env.DELAY || 5;
 
-    if (!_.isArray(entries)) {
+    if (_.isArray(entries)) {
+      entries.reverse();
+    } else {
       err = "JSON parse error.";
     }
 
     if (err) {
-      console.log('ERROR: %s Polling again in %s minutes for events after %s ...', err, delay++, startup);
+      console.log('ERROR: %s Polling again in %s minutes for events after %s ...', err, delay++, moment(startup).format('LLL'));
       setTimeout(poll, delay * 60 * 1000);
     } else {
-      async.each(entries, function(entry, callback) {
-        var incoming = !!entry.incoming_message_id,
-            ts = moment(entry.created_at).valueOf(),
-            url = feedUrl.protocol + '//' + feedUrl.host + '/request_event/' + entry.id,
-            body = _s.prune(entry.public_body.short_name || entry.public_body.name, 30, ''),
-            title = _s.prune(entry.info_request.title, 60, ''),
-            user = _s.prune(entry.user.name, 30, ''),
-            status = (entry.display_status || '').replace(/\.$/, ''),
-            line;
-
-        if (entry.event_type === 'comment' || !status) {
-          return callback();
-        }
-        if (incoming) {
-          line = url + ' [' + status + '] ' + body + ' replied about ' + title;
-        } else {
-          line = url + ' [' + status + '] ' + user + ' about ' + title;
-        }
-
-        if (ts > startup) {
-          latest = ts;
-          twit.updateStatus(line.substring(0, 140), function(err) {
-            if (err) {
-              startup = latest;
-            }
-            callback(err);
-          });
-        } else {
-          callback();
-        }
-      }, function(err) {
-        if (err) {
-          delay++;
-          console.log(err);
-        } else {
-          startup = latest;
-          delay = process.env.DELAY || 5;
-        }
-
-        console.log('polling again in ' + delay + ' minutes for events after ' + startup + ' ...');
-        setTimeout(poll, delay * 60 * 1000);
-      });
+      queue.push(entries);
     }
   });
+}
+
+function processEntry(entry, callback) {
+  var incoming = !!entry.incoming_message_id,
+      ts = moment(entry.created_at).valueOf(),
+      url = feedUrl.protocol + '//' + feedUrl.host + '/request_event/' + entry.id,
+      body = _s.prune(entry.public_body.short_name || entry.public_body.name, 30, ''),
+      title = entry.info_request.title,
+      user = _s.prune(entry.user.name, 30, ''),
+      status = (entry.display_status || '').replace(/\.$/, ''),
+      line;
+
+  if (entry.event_type === 'comment' || !status) {
+    return callback();
+  }
+
+  if (incoming) {
+    line = _s.sprintf('%s [%s] %s replied about %s', url, status, body, title);
+  } else {
+    line = _s.sprintf('%s [%s] %s about %s', url, status, user, title);
+  }
+
+  if (ts > startup) {
+    if (ts > latest) {
+      latest = ts;
+    }
+    if (process.env.NODE_ENV === 'production') {
+      twit.updateStatus(_s.prune(line, 140), callback);
+    } else {
+      console.log(_s.prune(line, 140));
+      callback();
+    }
+  } else {
+    callback();
+  }
 }
 
 if (process.env.FEED_URL) {
